4b4d91247396a1546f2d15cfe3aff3c72a4a3f21
ciazhar <daisetsunakama@gmail.com>
2017-08-25 18:41:03 +0700

    change class to nol final

--- src/main/kotlin/com/ciazhar/springkotlinrest/SpringKotlinRestApplication.kt
+++ src/main/kotlin/com/ciazhar/springkotlinrest/SpringKotlinRestApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class SpringKotlinRestApplication
+open class SpringKotlinRestApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(SpringKotlinRestApplication::class.java, *args)
---
+++ src/main/kotlin/com/ciazhar/springkotlinrest/controller/CostumerController.kt
@@ -0,0 +1,5 @@
+package com.ciazhar.springkotlinrest.controller
+
+/**
+ * Created by ciazhar on 8/25/17.
+ */
