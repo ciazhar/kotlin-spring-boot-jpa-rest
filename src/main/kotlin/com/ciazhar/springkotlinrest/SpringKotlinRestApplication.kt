a8358babcb518e971108aada664b92bfcee9e31a
ciazhar <daisetsunakama@gmail.com>
2017-08-25 11:33:33 +0700

    starter project

---
+++ src/main/kotlin/com/ciazhar/springkotlinrest/SpringKotlinRestApplication.kt
@@ -0,0 +1,11 @@
+package com.ciazhar.springkotlinrest
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpringKotlinRestApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(SpringKotlinRestApplication::class.java, *args)
+}
