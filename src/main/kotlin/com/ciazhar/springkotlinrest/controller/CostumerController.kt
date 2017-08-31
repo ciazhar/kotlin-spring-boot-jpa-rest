4a6e02f051f31675b525aa1cd8b1b876c59bf0b6
ciazhar <daisetsunakama@gmail.com>
2017-08-31 22:12:26 +0700

    update costumer controller

--- src/main/kotlin/com/ciazhar/springkotlinrest/controller/CostumerController.kt
+++ src/main/kotlin/com/ciazhar/springkotlinrest/controller/CostumerController.kt
@@ -1,10 +1,12 @@
 package com.ciazhar.springkotlinrest.controller
 
 import com.ciazhar.springkotlinrest.model.Costumer
+import com.ciazhar.springkotlinrest.model.dto.request.RegisterForm
+import com.ciazhar.springkotlinrest.model.dto.response.ResponseData
 import com.ciazhar.springkotlinrest.repository.CostumerRepository
+import com.ciazhar.springkotlinrest.service.CostumerService
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
-
 
 /**
  * Created by ciazhar on 8/25/17.
@@ -12,18 +14,33 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping("/costumer")
-class CostumerController(val repository: CostumerRepository){
+class CostumerController(val repository: CostumerRepository, val service : CostumerService){
 
-    @RequestMapping("/all")
-    fun findAll() = repository.findAll()
+    @GetMapping("/all")
+    fun findAll() : ResponseData<*> {
+        return ResponseData(repository.findAll())
+    }
 
-    @RequestMapping("/save")
-    fun save(@RequestBody @Valid costumer : Costumer)= repository.save(costumer)
+    @GetMapping("/single")
+    fun single(@RequestParam id : Int) : ResponseData<*>{
+        return ResponseData(repository.findOne(id))
+    }
 
-    @RequestMapping("/delete")
-    fun delete(@RequestParam id : Int) = repository.delete(id)
+    @PostMapping("/register")
+    fun register(@RequestBody @Valid register: RegisterForm) : ResponseData<*>{
+        return service.register(register)
+    }
 
-    @RequestMapping("/single")
-    fun string(@RequestParam id : Int) = repository.findOne(id)
+    @PutMapping("/update")
+    fun update(@RequestBody @Valid costumer: Costumer) : ResponseData<*>{
+        return service.update(costumer)
+    }
+
+    @DeleteMapping("/delete")
+    fun delete(@RequestParam id : Int) : ResponseData<*> {
+        return ResponseData(repository.delete(id))
+    }
+
+
 
 }
